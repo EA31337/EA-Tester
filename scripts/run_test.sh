8a9a39cae9425f31c5b3973ccaecd1e7880db4b5
Rafal <kenorb@gmail.com>
2015-08-14 00:44:05 +0100

    Added EA downloading and changing ini file in-place.

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# Enable globbing.
 shopt -s globstar
+
+# Download EA.
+wget -NP /opt/**/experts "https://www.dropbox.com/s/gukjb229sv2ilpn/MACD.ex4"
+
+# Update configuration.
+ex -s +"%s/^TestExpert=\zs.\+$/MACD/" -cwq /vagrant/conf/mt4-tester.ini
+
+# Monitor logs in the background.
+sleep 5 && tail -f /opt/**/*.log &
+
+# Run terminal.
 wine /opt/**/terminal.exe /vagrant/conf/mt4-tester.ini 
