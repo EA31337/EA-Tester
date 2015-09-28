8cffdbecf559d0de8c179ff7a63763ed8316e55b
Adrian Kierzkowski <adrian.kierzkowski@gmail.com>
2015-09-28 19:06:39 +0200

    Added run_backtest.sh file. Please start from the line 30 of run_test.sh. Hashes should be changed to "@" or something: ex -s +"%s#^TestReport=\zs.\+$#$REPORTDIR#" -cwq /vagrant/conf/mt4-tester.ini

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
+REPORTDIR="$1"
+SETFILE="$2"
+MODE="$3"
+SYMBOL="$4"
+DEPOSIT="$5"
+YEAR="$6"
+SPREAD="$7"
+SOURCE="$8"
+
+EXPERT=EA31337-$MODE
+FROM=$YEAR.01.01
+TO=$YEAR.01.02
+
 # Enable globbing.
 shopt -s globstar
 
 # Download EA.
-wget -NP /opt/**/MQL4/Experts "https://www.dropbox.com/s/gukjb229sv2ilpn/MACD.ex4"
+wget -NP /opt/**/MQL4/Experts "https://www.dropbox.com/s/gukjb229sv2ilpn/MACD.ex4" # -O "/opt/MetaTrader 4/MQL4/Experts/EA31337-Advanced.ex4"
+
+cp /vagrant/MACD.ex4 "/opt/MetaTrader 4/MQL4/Experts/EA31337-Advanced.ex4"
 
 # Update configuration.
-ex -s +"%s/^TestExpert=\zs.\+$/MACD/" -cwq /vagrant/conf/mt4-tester.ini
+ex -s +"%s/^TestExpert=\zs.\+$/$EXPERT/" -cwq /vagrant/conf/mt4-tester.ini
+ex -s +"%s/^TestExpertParameters=\zs.\+$/$SETFILE/" -cwq /vagrant/conf/mt4-tester.ini
+ex -s +"%s/^TestSymbol=\zs.\+$/$SYMBOL/" -cwq /vagrant/conf/mt4-tester.ini
+ex -s +"%s/^TestFromDate=\zs.\+$/$FROM/" -cwq /vagrant/conf/mt4-tester.ini
+ex -s +"%s/^TestToDate=\zs.\+$/$TO/" -cwq /vagrant/conf/mt4-tester.ini
+ex -s +"%s#^TestReport=\zs.\+$#$REPORTDIR#" -cwq /vagrant/conf/mt4-tester.ini
+
+cat /vagrant/conf/mt4-tester.ini | grep TestReport
+
+ex -s +"%s/^TestReplaceReport=\zs.\+$/true/" -cwq /vagrant/conf/mt4-tester.ini
 
 # Copying ini file to MT's directory so MT can find it.
 sudo cp /vagrant/conf/mt4-tester.ini /opt/MetaTrader\ 4/mt4-tester.ini
