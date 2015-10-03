eac55a2078e695722526ddb645e5dac706a3b203
kenorb <kenorb@gmail.com>
2015-10-04 00:24:21 +0100

    Converted run_backtest to support command-line parameters.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -5,7 +5,7 @@
 #
 
 # Initialize script.
-set -x
+set -xe
 if [ ! -d "/vagrant/scripts" ]; then
   echo "This script needs to be run within vagrant VM."
   exit 1
@@ -17,13 +17,6 @@ shopt -s globstar # Enable globbing.
 # Perform an unattended installation of a Debian packages.
 ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
 dpkg-reconfigure debconf -f noninteractive -p critical
-
-# Configure locale (http://serverfault.com/a/500778/130437).
-#export LANGUAGE=en_US.UTF-8
-#export LANG=en_US.UTF-8
-#export LC_ALL=en_US.UTF-8
-#locale-gen en_US.UTF-8
-#dpkg-reconfigure locales
 
 # Install the locale packate to prevent an invalid locale.
 apt-get install -y language-pack-en
@@ -48,7 +41,7 @@ apt-get install -y libx11-dev libxtst-dev libxinerama-dev libxkbcommon-dev
 #curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 # Install xdotool.
-git clone https://github.com/jordansissel/xdotool && make -C xdotool
+#git clone https://github.com/jordansissel/xdotool && make -C xdotool
 
 # Run X virtual framebuffer on screen 0.
 Xvfb :0 -screen 0 1024x768x16 & # Run X virtual framebuffer on screen 0.
@@ -67,8 +60,5 @@ chown -R vagrant:vagrant /opt
 # Install VM specific binaries.
 install -v /vagrant/scripts/run_backtest.sh /usr/local/bin/run_backtest
 install -v /vagrant/scripts/run_optimizer.sh /usr/local/bin/run_optimizer
-
-# Append extra settings into bashrc file.
-ex +':$s@$@\ralias run_backtest=/vagrant/scripts/run_backtest.sh@' -cwq /etc/bash.bashrc
 
 echo "$0 done."
--- scripts/run_backtest.sh
+++ scripts/run_backtest.sh
@@ -4,52 +4,77 @@ CWD="$(cd -P -- "$(dirname -- "$0")/../" && pwd -P)"
 OUT="/opt"
 TPL="/vagrant/conf/mt4-tester.ini"
 
-REPORTDIR="$1"
-SETFILE="$2"
-MODE="$3"
-SYMBOL="$4"
-DEPOSIT="$5"
-YEAR="$6"
-SPREAD="$7"
-SOURCE="$8"
-FROM="$YEAR.01.01"
-TO="$YEAR.01.02"
-EXPERT="$(find "$CWD" '(' -name "*$MODE*.ex4" -or -name "*$MODE*.ex5" ')' -print -quit)"
+# Define functions.
+run_logs() {
+  # Monitor logs in the background.
+  find "$OUT" '(' -name "*.log" -or -name '*.dat' ')' -delete # Remove old log and dat files.
+  (sleep 5 && find . -name "*.log" | xargs tail -f) &
+  trap "killall tail" EXIT # Clean up after exit.
+}
+configure_wine() {
+# Configure wine.
+  export WINEDLLOVERRIDES="mscoree,mshtml=" # Disable gecko in wine.
+  export DISPLAY=:0.0 # Select screen 0.
+  export WINEDEBUG=warn+all # For debugging, try: WINEDEBUG=trace+all
+}
 
 # Check if terminal is present.
 [ "$(find "$OUT" -name terminal.exe -print -quit)" ] || $CWD/scripts/dl_mt4.sh
 TERMINAL_EXE="$(find "$OUT" -name terminal.exe -print -quit)"
 TERMINAL_DIR="$(dirname "$TERMINAL_EXE")"
 TERMINAL_INI="$TERMINAL_DIR/config/mt4-tester.ini"
-echo $TERMINAL_EXE
-echo $TERMINAL_DIR
-echo $TERMINAL_INI
 
-# Check if backtest files are present.
-[ "$(find "$OUT" -name '*.fxt')" ] || $CWD/scripts/dl_bt_data.sh
-
-# Download EA.
-$CWD/scripts/dl_ea.sh
-
-# Copy and update the configuration files, so platform can find it.
+# Copy the configuration file, so platform can find it.
 cp -v "$TPL" "$TERMINAL_INI"
-[ -s "$SETFILE" ] && cp -v "$SETFILE" "$TERMINAL_DIR/tester"
-[ "$EXPERT" ]     && ex -s +"%s/^TestExpert=\zs.\+$/$EXPERT/" -cwq "$TERMINAL_INI"
-[ "$SETFILE" ]    && ex -s +"%s/^TestExpertParameters=\zs.\+$/$SETFILE/" -cwq "$TERMINAL_INI"
-[ "$SYMBOL" ]     && ex -s +"%s/^TestSymbol=\zs.\+$/$SYMBOL/" -cwq "$TERMINAL_INI"
-[ "$FROM" ]       && ex -s +"%s/^TestFromDate=\zs.\+$/$FROM/" -cwq "$TERMINAL_INI"
-[ "$TO" ] 	      && ex -s +"%s/^TestToDate=\zs.\+$/$TO/" -cwq "$TERMINAL_INI"
-[ "$REPORTDIR" ]  && ex -s +"%s#^TestReport=\zs.\+$#$REPORTDIR#" -cwq "$TERMINAL_INI"
 
-# Monitor logs in the background.
-find "$OUT" '(' -name "*.log" -or -name '*.dat' ')' -delete
-sleep 5 && tail -f $TERMINAL_DIR/*/*.log &
-trap "killall tail" EXIT # Clean up after exit.
+# Parse the arguments.
+while getopts r:f:n:p:d:y:s:b: opts; do
+  case ${opts} in
+    r) # Directory to save the Reports files.
+      REPORTDIR=${OPTARG}
+      [ "$REPORTDIR" ]  && ex -s +"%s#^TestReport=\zs.\+$#$REPORTDIR#" -cwq "$TERMINAL_INI"
+      ;;
+    f) # The set file to run the test.
+      SETFILE=${OPTARG}
+      [ -s "$SETFILE" ] && cp -v "$SETFILE" "$TERMINAL_DIR/tester"
+      [ "$SETFILE" ]    && ex -s +"%s/^TestExpertParameters=\zs.\+$/$SETFILE/" -cwq "$TERMINAL_INI"
+      ;;
+    n) # EA name.
+      NAME=${OPTARG}
+      EXPERT="$(find "$CWD" '(' -name "*$NAME*.ex4" -or -name "*$NAME*.ex5" ')' -print -quit)"
+      $CWD/scripts/dl_ea.sh # Download EA.
+      [ "$EXPERT" ]     && ex -s +"%s/^TestExpert=\zs.\+$/$EXPERT/" -cwq "$TERMINAL_INI"
+      ;;
+    p) # Symbol pair to test.
+      SYMBOL=${OPTARG}
+      [ "$SYMBOL" ]     && ex -s +"%s/^TestSymbol=\zs.\+$/$SYMBOL/" -cwq "$TERMINAL_INI"
+      ;;
+    d) # Deposit amount to test.
+      DEPOSIT="$5"
+      # @todo: Set the right deposit for the test.
+      ;;
+    y) # Year to test.
+      YEAR=${OPTARG}
+      FROM="$YEAR.01.01"
+      TO="$YEAR.01.02"
+      [ "$FROM" ]       && ex -s +"%s/^TestFromDate=\zs.\+$/$FROM/" -cwq "$TERMINAL_INI"
+      [ "$TO" ] 	      && ex -s +"%s/^TestToDate=\zs.\+$/$TO/" -cwq "$TERMINAL_INI"
+      ;;
+    s) # Spread to test.
+      SPREAD=${OPTARG}
+      # @todo: Set the right spread for the test.
+      ;;
+    b) # Backtest data to test.
+      BT_SOURCE=${OPTARG}
+      # @todo: Place the right backtest data into the right place and change the profile name.
+      [ "$(find "$OUT" -name '*.fxt')" ] || $CWD/scripts/dl_bt_data.sh # Download backtest files if not present.
+      ;;
+  esac
+done
 
-# Configure wine.
-export WINEDLLOVERRIDES="mscoree,mshtml=" # Disable gecko in wine.
-export DISPLAY=:0.0 # Select screen 0.
-export WINEDEBUG=warn+all # For debugging, try: WINEDEBUG=trace+all
+# Prepare before test run.
+run_logs
+configure_wine
 
-# Run terminal.
-wine $TERMINAL_EXE $TERMINAL_INI && html2text $TERMINAL_DIR/Report.htm
+# Run the test in the platform.
+wine "$TERMINAL_EXE" "$TERMINAL_INI" && html2text "$TERMINAL_DIR/Report.htm"
