f07577994ca8711b768d1e4b78de597710c15186
kenorb <kenorb@gmail.com>
2015-11-15 22:44:46 +0000

    Several fixes for running backtest script.

--- scripts/run_backtest.sh
+++ scripts/run_backtest.sh
@@ -5,30 +5,21 @@ CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 type git realpath ex
 
 # Define functions.
-input_set() {
-  key="$1"
-  value="$2"
-  file="${3:SETFILE}"
-  ex -s +"%s/$key=\zs.\+$/$value/" -cwq "$file"
-}
-
-ini_set() {
-  key="$1"
-  value="$2"
-  file="${3:TERMINAL_INI}"
-  ex -s +"%s/^$key=\zs.\+$/$value/" -cwq "$file"
-}
-
 on_success() {
+  ! grep -C2 -e "Initialization failed" <(show_logs) || exit 1
   echo "Test succeded."
   show_logs
-  html2text "$(find "$TERMINAL_DIR" -name "Report*.htm")"
-  [ "$DEST" ] && find "$TERMINAL_DIR" -name "*Report*" -and -not -path "*templates/*" -execdir cp -v "{}" "$DEST" ';'
+  find "$TERMINAL_DIR" -name "Report*.htm" -exec sh -c "html2text '{}' | head -n40"  ';'
+  [ "$DEST" ] && find "$TERMINAL_DIR" -name "Report*" -execdir cp -v "{}" "$DEST" ';'
+  on_finish
+  exit 0
 }
 
 on_failure() {
   echo "Test failed."
   show_logs
+  on_finish
+  exit 1
 }
 
 on_finish() {
@@ -44,11 +35,11 @@ echo "Checking platform dependencies..."
 . $CWD/.configrc
 
 # Copy the configuration file, so platform can find it.
-cp -v "$TPL" "$TERMINAL_INI"
-cp -v "$TPL2" "$TERMINAL_INI2"
+cp -v "$TPL_TEST" "$TESTER_INI"
+cp -v "$TPL_TERM" "$TERMINAL_INI"
 
 # Parse the arguments.
-while getopts r:f:n:p:d:y:s:b:D: opts; do
+while getopts r:f:n:E:p:d:y:s:b:D: opts; do
   case ${opts} in
     r) # The name of the test report file. A relative path can be specified
       if [ -s "$(dirname "${OPTARG}")" ]; then # If base folder exists,
@@ -56,55 +47,56 @@ while getopts r:f:n:p:d:y:s:b:D: opts; do
       else
         REPORT="$(basename "${OPTARG}")" # ... otherwise, it's a filename.
       fi
-      [ "$REPORT" ] && ex -s +"%s#^TestReport=\zs.\+\$#$REPORT#" -cwq "$TERMINAL_INI"
-# ini_set "^TestReport" "$REPORT" $TERMINAL_INI # Test me.
+      ini_set "^TestReport" "$REPORT" "$TESTER_INI"
       ;;
 
     f) # The set file to run the test.
       SETFILE=${OPTARG}
       [ -s "$SETFILE" ] && cp -v "$SETFILE" "$TERMINAL_DIR/tester"
-      [ "$SETFILE" ]    && ex -s +"%s/^TestExpertParameters=\zs.\+$/$SETFILE/" -cwq "$TERMINAL_INI"
-# ini_set "^TestExpertParameters" "$SETFILE" $TERMINAL_INI # Test me.
+      ini_set "^TestExpertParameters" "$SETFILE" "$TESTER_INI"
       ;;
 
     n) # EA name.
       EA_NAME=${OPTARG}
       EA_PATH="$(find "$ROOT" '(' -name "*$EA_NAME*.ex4" -o -name "*$EA_NAME*.ex5" ')' -print -quit)"
       [ -s "$EA_PATH" ] && { cp -v "$EA_PATH" "$TERMINAL_DIR/MQL4/Experts"; EA_NAME="$(basename "$EA_PATH")"; }
-      [ "$EA_NAME" ]    && ex -s +"%s/^TestExpert=\zs.\+$/$EA_NAME/" -cwq "$TERMINAL_INI"
+      ini_set "^TestExpert" "$EA_NAME" "$TESTER_INI"
+      ;;
+
+    E) # EA settings.
+      EA_OPTS=${OPTARG}
+      EA_NAME="$(ini_get TestExpert)"
+      echo "$EA_OPTS" >> "$TESTER_DIR/${EA_NAME}.ini"
       ;;
 
     p) # Symbol pair to test.
       SYMBOL=${OPTARG}
-      [ "$SYMBOL" ] && ex -s +"%s/^TestSymbol=\zs.\+$/$SYMBOL/" -cwq "$TERMINAL_INI"
+      ini_set "^TestSymbol" "$SYMBOL" "$TESTER_INI"
+      # EA_NAME="$(ini_get TestExpert)"
+      # echo "currency=$SYMBOL" >> "$TESTER_DIR/${EA_NAME}.ini"
       ;;
 
     d) # Deposit amount to test.
       DEPOSIT=${OPTARG}
-      if [ -n "$DEPOSIT" ]; then
-        DEPOSIT_PATTERN="Deposit="
-        grep -q "^$DEPOSIT_PATTERN" "$TERMINAL_INI" && sed "s/\(^$DEPOSIT_PATTERN\).*/\1$DEPOSIT/" -i "$TERMINAL_INI" \
-                                                    || echo -e "\n; Set deposit amount to test\n$DEPOSIT_PATTERN$DEPOSIT" >> "$TERMINAL_INI"
-      fi
+      EA_NAME="$(ini_get TestExpert)"
+      echo "deposit=$DEPOSIT" >> "$TESTER_DIR/${EA_NAME}.ini"
       ;;
 
     y) # Year to test.
       YEAR=${OPTARG}
-      FROM="$YEAR.01.01"
-      TO="$YEAR.01.02"
-      [ "$FROM" ] && ex -s +"%s/^TestFromDate=\zs.\+$/$FROM/" -cwq "$TERMINAL_INI"
-      [ "$TO" ]   && ex -s +"%s/^TestToDate=\zs.\+$/$TO/" -cwq "$TERMINAL_INI"
+      ini_set "^TestFromDate" "$YEAR.01.01" "$TESTER_INI"
+      ini_set "^TestToDate"   "$YEAR.01.30" "$TESTER_INI"
       ;;
 
     s) # Spread to test.
       SPREAD=${OPTARG}
-      [ "$SPREAD" ] && ex -s +"%s/^Spread=\zs.\+$/$SPREAD/" -cwq "$TERMINAL_INI2"
+      ini_set "^Spread" "$SPREAD" "$TERMINAL_INI"
       ;;
 
     b) # Backtest data to test.
       BT_SOURCE=${OPTARG}
       # Download backtest files if not present.
-      [ "$(find "$TERMINAL_DIR" -name '*.fxt')" ] || $SCR/dl_bt_data.sh ${SYMBOL:EURUSD} ${YEAR:2014} DS
+      [ "$(find "$TERMINAL_DIR" -name '*.fxt')" ] || $SCR/dl_bt_data.sh ${SYMBOL:-EURUSD} ${YEAR:-2014} DS
       ;;
 
     D) # Destination directory to save test results.
@@ -118,5 +110,5 @@ done
 clean_files
 
 # Run the test with the platform.
-time wine "$TERMINAL_EXE" "config/$CONF" && on_success || on_failure
-on_finish
+set -x
+time wine "$TERMINAL_EXE" "config/$CONF_TEST" && on_success || on_failure
