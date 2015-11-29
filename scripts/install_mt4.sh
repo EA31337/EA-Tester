fecf5859b587743363faf58788b5660125b41b93
kenorb <kenorb@gmail.com>
2015-11-29 20:51:30 +0000

    Simplifying MT4 installer.

--- scripts/install_mt4.sh
+++ scripts/install_mt4.sh
@@ -5,52 +5,35 @@ CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 set -e
 type wget xdotool
 
-# Set delay for periodic checks
-DELAY=5
+# Initialize settings.
+. $CWD/.configrc
 
 echo "Downloading MT4 installer..."
-wget -ct3 https://download.mql5.com/cdn/web/metaquotes.software.corp/mt4/mt4setup.exe
+wget -P "$OUT" -ct3 https://download.mql5.com/cdn/web/metaquotes.software.corp/mt4/mt4setup.exe
 
 echo "Starting MT4 Setup in Wine..."
-wine mt4setup.exe &
+wine "$OUT/mt4setup.exe" &
 
-# Wait until Wine initializes
-while : ; do
-	echo "Waiting for Wine to initialize..."
-	sleep $DELAY
-	set +e  # Workaround for xdotool issue #60
-	WINDOW_ID=$(xdotool search --name "MetaTrader 4 Setup*")
-	set -e  # Workaround for xdotool issue #60
-	[[ -z $WINDOW_ID ]] || break
+echo "Waiting for Wine to initialize..."
+while ! WID=$(xdotool search --name "MetaTrader 4 Setup"); do
+  sleep 1
 done
 
-# Set focus on installer window and act to install platform
 echo "Sending installer keystrokes..."
-xdotool key --window $WINDOW_ID --delay 500 space Tab Tab Tab Return Tab Tab Tab space Alt+n
+xdotool key --window $WID --delay 500 space Tab Tab Tab Return Tab Tab Tab space Alt+n
 
-# Wait until installation is ready
-while : ; do
-	echo "Installing MT4 platform..."
-	sleep $DELAY
-	set +e  # Avoid script termination
-	PS=$(ps | grep "mt4setup.exe")
-	set -e  # Avoid script termination
-	[[ ! -z $PS ]] || break
-done
+echo "Waiting for installer to finish..."
+while pgrep -l mt4setup; do sleep 5; xwininfo -id $WID -tree; done
 
-# Wait for MT4 to start
-while : ; do
-	echo "Waiting for MT4 platform to start..."
-	sleep $DELAY
-	set +e  # Workaround for xdotool issue #60
-	WINDOW_ID=$(xdotool search -name "MetaTrader 4 - *")
-	set -e  # Workaround for xdotool issue #60
-	[[ -z $WINDOW_ID ]] || break
+echo "Waiting for MT4 platform to start..."
+while ! WID=$(xdotool search --name "MetaTrader 4 -"); do
+  sleep 1
 done
 
 # Close running MT4 instance, first the two login popup window, secondly application itself
 echo "Sending application closer keystrokes..."
-xdotool key --window $WINDOW_ID --delay 500 Escape Escape Alt+f x
+xdotool key --window $WID --delay 500 Escape Escape Alt+f x
+wineserver -k
 
 # Re-initialize settings.
 . $CWD/.configrc
