99212753e3f070139958f5a678842d69c246fd79
kenorb <kenorb@users.noreply.github.com>
2016-04-09 03:40:47 +0100

    Added eval.sh script.

---
+++ scripts/eval.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Script to invoke custom code.
+# Usage: eval.sh code to invoke
+# E.g.: eval.sh check_logs ExpertRemove
+CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+# Initialize settings.
+. $CWD/.configrc
+eval $@
