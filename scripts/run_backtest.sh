740aca1f3351ce45d58e9397826dd2cd5f742ce0
kenorb <kenorb@users.noreply.github.com>
2016-05-18 03:08:59 +0100

    Check log for Error phrase for failing scenarion.

--- scripts/run_backtest.sh
+++ scripts/run_backtest.sh
@@ -19,6 +19,7 @@ on_success() {
   ! check_logs ".\+ no history data" || { rm $VFLAG "$CUSTOM_INI"; exit 1; }
   ! check_logs ".\+ cannot start" || exit 1
   ! check_logs ".\+ cannot open" || exit 1
+  ! check_logs "Error: .\+" || exit 1
   echo "TEST succeeded." >&2
   parse_results $@
   on_finish
