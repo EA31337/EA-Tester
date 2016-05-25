e5d74c484f7aa91042451e800d2ba007c4699b4f
kenorb <kenorb@users.noreply.github.com>
2016-05-25 23:39:46 +0100

    Added ini_set_ea wrapper to set custom value in the EA ini file.

--- scripts/run_backtest.sh
+++ scripts/run_backtest.sh
@@ -230,7 +230,7 @@ while getopts $ARGS arg; do
       IFS='='; ea_option=($EA_OPTS)
       IFS=$' \t\n' # Restore IFS.
       [ -f "$EA_INI" ]
-      ini_set "^${ea_option[0]}" "${ea_option[1]}" "$EA_INI"
+      ini_set_ea "^${ea_option[0]}" "${ea_option[1]}"
       ;;
 
     c) # Base currency for test (e.g. USD).
