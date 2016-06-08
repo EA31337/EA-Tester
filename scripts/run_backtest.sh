d2115a9766e48708057827085ccf6e23a1ba06b4
kenorb <kenorb@users.noreply.github.com>
2016-06-08 02:42:25 +0100

    Re-phrased cannot calculate catch phrase. [skip ci]

--- scripts/run_backtest.sh
+++ scripts/run_backtest.sh
@@ -19,7 +19,7 @@ on_success() {
   ! check_logs ".\+ no history data" || { rm $VFLAG "$CUSTOM_INI"; exit 1; }
   ! check_logs ".\+ cannot start" || exit 1
   ! check_logs ".\+ cannot open" || exit 1
-  ! check_logs ".\+ cannot calculate" || exit 1
+  ! check_logs ".\+ rate cannot" || exit 1 # E.g. Tester: exchange rate cannot be calculated
   ! check_logs "Error: .\+" || exit 1
   echo "TEST succeeded." >&2
   parse_results $@
