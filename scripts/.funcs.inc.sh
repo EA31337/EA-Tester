99709265a423fc4f90a94efaeb027428e42a4142
kenorb <kenorb@users.noreply.github.com>
2016-06-14 11:39:44 +0100

    Added function to check the version of the given binary file.

--- scripts/.funcs.inc.sh
+++ scripts/.funcs.inc.sh
@@ -511,8 +511,8 @@ install_mt() {
 
 ## Install filever
 install_filever() {
-  type wget cabextract install wine
-  wine filever && return
+  type wget cabextract install wine >&2
+  wine filever >& /dev/null && return
   local tools_url="http://web.archive.org/https://download.microsoft.com/download/d/3/8/d38066aa-4e37-4ae8-bce3-a4ce662b2024/WindowsXP-KB838079-SupportTools-ENU.exe"
   local dtmp=$(mktemp -d)
   echo "Installing filever tool..." >&2
@@ -523,6 +523,16 @@ install_filever() {
   install -v filever.exe ~/.wine/drive_c/windows
   rm -fr "$dtmp"
   cd -
+}
+
+# Check the version of the given binary file.
+# Usage: filever terminal.exe
+filever() {
+  type awk >&2
+  wine filever >& /dev/null || install_filever
+  local file=$1
+  find "$PWD" "$TERMINAL_DIR" -type f -name "$file" -execdir wine filever /v "$file" ';' -quit \
+    | grep ProductVersion | awk '{print $2}'
 }
 
 ## Clean up.
