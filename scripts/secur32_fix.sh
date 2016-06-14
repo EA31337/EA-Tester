5a8189211b9a6e49a5bf716fb008602df9ddef2b
kenorb <kenorb@users.noreply.github.com>
2016-06-14 02:39:56 +0100

    Added missing sudo for cp

--- scripts/secur32_fix.sh
+++ scripts/secur32_fix.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 
 # Check whether libgnutls needs patching.
@@ -6,10 +7,11 @@ echo "Checking version of secur32.dll.so..." >&2
 if [ -d "/usr/lib/i386-linux-gnu" ]; then
   CHANNEL_PATH=$(find /usr/lib/i386-linux-gnu -type f -name 'secur32.dll.so')
   if [[ -n $(strings "$CHANNEL_PATH" 2> /dev/null | grep 'libgnutls.so.26') ]]; then
+    which timeout 2> /dev/null && timeout 2 sudo id
     echo "Found that Wine's secur32 channel is using libgnutls26 at '$CHANNEL_PATH'." >&2
     echo "Installing libgnutls28..." >&2
     sudo apt-get -qy install libgnutls28:i386
     echo 'Overwriting channel with the new build for wine-1.8.0 and libgnutls28...' >&2
-    cp -vf "$CWD/../lib/secur32.dll.so" "$CHANNEL_PATH" && echo "The secur32.dll.so has been patched." >&2
+    sudo cp -vf "$CWD/../lib/secur32.dll.so" "$CHANNEL_PATH" && echo "The secur32.dll.so has been patched." >&2
   fi
 fi
