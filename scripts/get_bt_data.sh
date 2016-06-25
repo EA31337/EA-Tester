1cf3cd410140a8450edbe36f7798901259d4a086
kenorb <kenorb@users.noreply.github.com>
2016-06-25 20:23:26 +0100

    Correcting logic when custom ini does not exist

--- scripts/get_bt_data.sh
+++ scripts/get_bt_data.sh
@@ -107,7 +107,8 @@ esac
 csv2data
 
 # Store the backtest data type.
-[ ! -f "$CUSTOM_INI" ] && touch "$CUSTOM_INI"
-ini_set "bt_data" "$bt_key" "$CUSTOM_INI" || echo "bt_data=$bt_key" >> "$CUSTOM_INI"
+[ -f "$CUSTOM_INI" ] \
+  && ini_set "bt_data" "$bt_key" "$CUSTOM_INI" \
+  || echo "bt_data=$bt_key" > "$CUSTOM_INI"
 
 echo "$0 done."
