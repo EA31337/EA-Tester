a174a7de83e62c1854b577bb48a604c9e94fa404
kenorb <kenorb@users.noreply.github.com>
2016-06-28 23:09:44 +0100

    Check for TestGenerator: write file error

--- scripts/run_backtest.sh
+++ scripts/run_backtest.sh
@@ -26,6 +26,7 @@ on_success() {
   ! check_logs ".\+ cannot open" || exit 1
   ! check_logs ".\+ rate cannot" || exit 1 # E.g. Tester: exchange rate cannot be calculated
   ! check_logs ".\+ not initialized" || exit 1
+  ! check_logs ".\+ file error" || exit 1
   ! check_logs "Error: .\+" || exit 1
   echo "TEST succeeded." >&2
   parse_results $@
