f4814835833a2cf1ff3131c2c339feda00e1b49b
kenorb <kenorb@users.noreply.github.com>
2016-07-01 01:46:15 +0100

    Adds fail on Configuration issue (initialization handler has returned a nonzero value) message

--- scripts/run_backtest.sh
+++ scripts/run_backtest.sh
@@ -29,6 +29,7 @@ on_success() {
   ! check_logs ".\+ file error" || exit 1
   ! check_logs "stop button .\+" || exit 1
   ! check_logs "Error: .\+" || exit 1
+  ! check_logs "Configuration issue .\+" || exit 1
   echo "TEST succeeded." >&2
   parse_results $@
   on_finish
