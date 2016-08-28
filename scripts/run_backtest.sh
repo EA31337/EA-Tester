c43a8232f65dcbcd22523b164386ee1b703457b0
kenorb <kenorb@users.noreply.github.com>
2016-08-28 02:24:15 +0100

    Move first 3 parameters into last column in report file

--- scripts/run_backtest.sh
+++ scripts/run_backtest.sh
@@ -80,8 +80,13 @@ parse_results() {
       t) # Convert test report file into brief text format.
         REPORT_TXT="$(dirname "$REPORT_HTM")/$REPORT_BASE.txt"
         echo "Converting HTML report ($(basename "$REPORT_HTM")) into short text file ($(basename "$REPORT_TXT"))..." >&2
-        # sed 's/ title="\(.*;\).*"\(.*\)<\/tr>/\2<td>\1<\/td><\/tr>/g'
-        grep -v mso-number "$REPORT_HTM" | html2text -nobs -width 105 | sed "/\[Graph\]/q" | grep -v '^\s.*;' > "$REPORT_TXT"
+        # Define pattern for moving first 3 parameters into last column.
+        move1_pattern='s/ title="\([0-9a-zA-Z=_]*; [0-9a-zA-Z=_]*; [0-9a-zA-Z=_]*;\).*"\(.*\)<\/tr>/\2<td>\1<\/td><\/tr>/g'
+        grep -v mso-number "$REPORT_HTM" | \
+          sed -e $move1_pattern | \
+          html2text -nobs -width 105 | \
+          sed "/\[Graph\]/q" | \
+          grep -v '^\s.*;' > "$REPORT_TXT"
         ;;
       T) # Convert test report file into full detailed text format.
         REPORT_TXT="$(dirname "$REPORT_HTM")/$REPORT_BASE.txt"
