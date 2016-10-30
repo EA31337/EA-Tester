75443bf88555f8eb7a8ae0d64b3af1fdfe54398d
kenorb <kenorb@users.noreply.github.com>
2016-10-30 15:22:32 +0000

    Creates LOG_DIR when does not exist

--- scripts/.funcs.inc.sh
+++ scripts/.funcs.inc.sh
@@ -598,7 +598,7 @@ install_mt() {
 
 # Validate platform dirs.
 validate_dirs() {
-  for dir in "$TESTER_DIR" "$TERMINAL_DIR/$MQL_DIR" "$EXPERTS_DIR" "$SCRIPTS_DIR" "$FILES_DIR" "$LIB_DIR"
+  for dir in "$TESTER_DIR" "$TERMINAL_DIR/$MQL_DIR" "$EXPERTS_DIR" "$SCRIPTS_DIR" "$FILES_DIR" "$LIB_DIR" "$LOG_DIR"
   do
     [ -d "$dir" ] || mkdir $VFLAG "$dir"
   done
