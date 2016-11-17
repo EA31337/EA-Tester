a607dc9ba61de81d350a73afa768388c11ab97df
kenorb <kenorb@users.noreply.github.com>
2016-11-17 21:02:54 +0000

    Corrects MetaTrader window title

--- scripts/install_mt4-xdot.sh
+++ scripts/install_mt4-xdot.sh
@@ -1,5 +1,5 @@
-#!/bin/sh -e
-# Script to install MT platform using xdotool.
+#!/usr/bin/env bash
+# Script to install MT platform using xdotool..
 CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 DTMP=$(mktemp -d)
 WURL="https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks"
@@ -20,9 +20,11 @@ wine "$HOME/mt4setup.exe" &
 
 set +e
 echo "Waiting for Wine to initialize..."
-while ! WID=$(xdotool search --name "MetaTrader 4 Setup"); do
+while ! WID=$(xdotool search --name "MetaTrader Setup"); do
   sleep 2
 done
+
+echo "Title: $(xdotool getwindowname $WID)..."
 
 echo "Sending installer keystrokes..." >&2
 xdotool key --window $WID --delay 500 space Tab Tab Tab Return Tab Tab Tab space Alt+n
@@ -32,11 +34,12 @@ xwininfo -id $WID -tree
 while pgrep -l mt4setup; do sleep 5; done
 
 echo "Waiting for MT4 platform to start..." >&2
-while ! WID=$(xdotool search --name "MetaTrader 4 - *"); do
+while ! WID=$(xdotool search --name "Account"); do
   sleep 2
 done
+xwininfo -id $WID -tree
 
-# Close running MT4 instance, first the two login popup window, secondly application itself
+# Close running MT4 instance, first the two login popup window, secondly application itself.
 echo "Closing application..." >&2
 xdotool key --window $WID --delay 500 Escape Escape Alt+f x
 sleep 1
