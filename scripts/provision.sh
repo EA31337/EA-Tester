bacb1a32a2bc5f49e9e2b68c142a2cf93ac31aa4
kenorb <kenorb@users.noreply.github.com>
2016-12-01 15:51:35 +0000

    Adds swapfile support if none (GH-110)

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -13,13 +13,12 @@ elif [ -f ~/.provisioned ]; then
   echo "Note: System already provisioned, skipping." >&2
   exit 0
 fi
-echo "OS Type: $OSTYPE"
+echo "OS Type: $(uname -a)"
 whoami && lsb_release -a && pwd
 type curl || apt-get -y install curl
 type dpkg apt-get
 
 # Init variables.
-ARCH=$(dpkg --print-architecture)
 id travis  && USER="travis"
 id vagrant && USER="vagrant"
 
@@ -28,40 +27,65 @@ GW=$(netstat -rn | grep "^0.0.0.0 " | cut -d " " -f10)
 curl -s localhost:3128 --connect-timeout 2 > /dev/null && export http_proxy="http://localhost:3128"
 curl -s       $GW:3128 --connect-timeout 2 > /dev/null && export http_proxy="http://$GW:3128"
 
-if type dpkg-reconfigure; then
+case "$(uname -s)" in
 
-    # Perform an unattended installation of a Debian packages.
-    export DEBIAN_FRONTEND=noninteractive
-    [ -f /etc/apt/apt.conf.d/70debconf ] && ex +"%s@DPkg@//DPkg" -scwq -V1 /etc/apt/apt.conf.d/70debconf
-    dpkg-reconfigure debconf -f noninteractive
-    echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+  Linux)
 
-    # Prepare wine dependencies.
-    sudo find /etc/apt -type f -name '*.list' -execdir sed -i 's/^\(deb-src\)/#\1/' {} +  # Omit source repositories from updates
+    if type dpkg-reconfigure; then
 
-    # Add i386 architecture for Wine
-    dpkg --add-architecture i386
-fi
+        # Perform an unattended installation of a Debian packages.
+        export DEBIAN_FRONTEND=noninteractive
+        [ -f /etc/apt/apt.conf.d/70debconf ] && ex +"%s@DPkg@//DPkg" -scwq -V1 /etc/apt/apt.conf.d/70debconf
+        dpkg-reconfigure debconf -f noninteractive
+        echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 
-# Add PPA/Wine repository
-add-apt-repository -y ppa:wine/wine-builds
+        # Prepare wine dependencies.
+        sudo find /etc/apt -type f -name '*.list' -execdir sed -i 's/^\(deb-src\)/#\1/' {} +  # Omit source repositories from updates
 
-# Update APT repositories
-apt-get -qq update
+        # Add i386 architecture for Wine
+        dpkg --add-architecture i386
+    fi
 
-# Install necessary packages
-apt-get install -qy language-pack-en                                          # Language pack to prevent an invalid locale.
-apt-get install -qy binutils coreutils moreutils cabextract zip unzip         # Common CLI utils.
-apt-get install -qy imagemagick                                               # ImageMagick.
-apt-get install -qy dbus                                                      # Required for Debian AMI on EC2.
-apt-get install -qy git realpath links html2text tree pv bc                   # Required commands.
-apt-get install -qy software-properties-common python-software-properties     # APT dependencies (required for a docker image).
-apt-get install -qy ca-certificates
+    # Add PPA/Wine repository
+    add-apt-repository -y ppa:wine/wine-builds
 
-# Install wine and dependencies.
-apt-get install -qy --install-recommends wine-staging winehq-staging
-apt-get install -qy xvfb xdotool x11-utils                                    # Wine from PPA/Wine and tools for MT4 installer.
-#apt-get install -qy libgnutls-dev                                            # GNU TLS library for secure connections.
+    # Update APT repositories
+    apt-get -qq update
+
+    # Install necessary packages
+    apt-get install -qy language-pack-en                                          # Language pack to prevent an invalid locale.
+    apt-get install -qy binutils coreutils moreutils cabextract zip unzip         # Common CLI utils.
+    apt-get install -qy imagemagick                                               # ImageMagick.
+    apt-get install -qy dbus                                                      # Required for Debian AMI on EC2.
+    apt-get install -qy git realpath links html2text tree pv bc                   # Required commands.
+    apt-get install -qy software-properties-common python-software-properties     # APT dependencies (required for a docker image).
+    apt-get install -qy ca-certificates
+
+    # Install wine and dependencies.
+    apt-get install -qy --install-recommends wine-staging winehq-staging
+    apt-get install -qy xvfb xdotool x11-utils                                    # Wine from PPA/Wine and tools for MT4 installer.
+    #apt-get install -qy libgnutls-dev                                            # GNU TLS library for secure connections.
+
+    # Setup swap file if none.
+    if [[ ! $(swapon -s) ]]; then
+      if [ -f /var/cache/swap/swapfile ]; then
+        swapon /var/cache/swap/swapfile
+      else
+        mkdir -pv /var/cache/swap
+        cd /var/cache/swap
+        dd if=/dev/zero of=swapfile bs=1K count=4M
+        chmod 600 swapfile
+        mkswap swapfile
+        swapon swapfile
+        cd -
+      fi
+    fi
+    ;;
+  Darwin)
+    brew install git
+    brew install wine
+  ;;
+esac
 
 # Set-up hostname.
 grep "$(hostname)" /etc/hosts && echo "127.0.0.1 $(hostname)" >> /etc/hosts
