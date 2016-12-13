509661ae43185bbc8a7d95706b1d991570d510fb
kenorb <kenorb@users.noreply.github.com>
2016-12-13 21:54:12 +0000

    Corrects git -q argument order

--- scripts/clone_repo.sh
+++ scripts/clone_repo.sh
@@ -8,5 +8,5 @@ REPO=$1
 DIR="$(basename "$REPO")"
 [ ! "$REPO" ] && { echo "Usage: $0 (git@repo/url)"; exit 1; }
 
-[ ! -d "$DIR" ] && git -q clone "$REPO" -b master --recursive --single-branch --depth 1
+[ ! -d "$DIR" ] && git clone -q "$REPO" -b master --recursive --single-branch --depth 1
 cd "$DIR" && git fetch origin && git status && git reset origin/master --hard
