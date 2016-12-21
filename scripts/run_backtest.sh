e12a2493bc7e8a0d1c5da680246c9ba69b08d747
kenorb <kenorb@users.noreply.github.com>
2016-12-21 21:26:28 +0000

    Adds 'deficient data' error

--- scripts/run_backtest.sh
+++ scripts/run_backtest.sh
@@ -29,6 +29,7 @@ on_success() {
   ! check_logs ".\+ not initialized" || exit 1
   ! check_logs ".\+ file error" || exit 1
   ! check_logs ".\+ data error" || exit 1
+  ! check_logs ".\+ deficient data" || exit 1
   ! check_logs "stop button .\+" || exit 1
   ! check_logs "Error: .\+" || exit 1
   ! check_logs "Configuration issue .\+" || exit 1
