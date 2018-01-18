ea507e08a78dc0a6364742b39d416c020fe27490
kenorb <kenorb@users.noreply.github.com>
2018-01-18 21:48:46 +0000

    Adds extra window info during installation

--- scripts/install_mt4-xdot.sh
+++ scripts/install_mt4-xdot.sh
@@ -4,22 +4,28 @@ CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 DTMP=$(mktemp -d)
 EXEFILE=mt4setup.exe
 export WINEDLLOVERRIDES="mscoree,mshtml=,winebrowser.exe="
-[ "$TRACE" ] && set -x
-set +e
 
 # Check the dependencies.
 type wget xdotool xwininfo wine ar >&2
 
+# Load the shell functions.
+. "$CWD/.funcs.inc.sh"
+
 echo "Downloading MT4 installer..." >&2
-[ ! -f "$HOME/$EXEFILE" ] && wget -P "$HOME" -ct3 ${MT_URL:-"https://download.mql5.com/cdn/web/8472/mt4/$EXEFILE"}
+[ ! -f "$HOME/$EXEFILE" ] \
+  && wget -P "$HOME" -ct3 ${MT_URL:-"https://download.mql5.com/cdn/web/8472/mt4/$EXEFILE"}
 
 echo "Starting MT4 Setup in Wine..." >&2
 [ -f "$HOME/$EXEFILE" ]
 wine "$HOME/$EXEFILE" &
 
+# Prints information of the window status in the background.
+live_stats &
+
 echo "Waiting for Wine to initialize..."
 while ! WID=$(xdotool search --name "MetaTrader 4 Setup"); do
-  sleep 2
+  winedbg --command "info wnd"
+  sleep 5
 done
 
 echo "Title: $(xdotool getwindowname $WID)..."
@@ -33,16 +39,17 @@ while pgrep -l mt4setup; do sleep 5; done
 
 echo "Waiting for MT4 platform to start..." >&2
 while ! WID=$(xdotool search --name "MetaTrader"); do
-  sleep 2
+  winedbg --command "info wnd $WID"
+  sleep 5
 done
 xwininfo -id $WID -tree
 
 # Close running MT4 instance, first the two login popup window, secondly application itself.
 echo "Closing application..." >&2
 xdotool key --window $WID --delay 500 Escape Escape Alt+f x
-sleep 1
+sleep 2
 wineserver -k
 
-find ~ /opt -name terminal.exe -print -quit
+find "$HOME" /opt -name terminal.exe -print -quit
 echo "Installation successful." >&2
 echo "${BASH_SOURCE[0]} done." >&2
