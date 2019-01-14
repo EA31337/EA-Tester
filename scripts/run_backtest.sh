c3ee18af34f4f021b2bcd75dd6becad2193efc46
kenorb <kenorb@users.noreply.github.com>
2019-01-14 22:27:36 +0000

    Improves usage and adds support for BT_TESTMODE

--- scripts/run_backtest.sh
+++ scripts/run_backtest.sh
@@ -30,86 +30,110 @@ Usage: $0 (args)
 
   -A (command)
     Action to evaluate (e.g. "file_get URL").
+    Variable (string): CODE
   -b (option)
-    Source of backtest data to test.
-    Default: DS
+    Source of backtest data to test. Default: DS
+    Variable (string): BT_SRC
   -B (filename)
     Specify early booting script to execute.
+    Variable (string): INCLUDE_BOOT
   -c (currency)
-    Base currency for test.
-    Default: USD
+    Base currency for test. Default: USD
+    Variable (string): BT_CURRENCY
   -C
     Clear previous backtest data files.
   -d (amount)
-    Deposit amount to test (e.g. 2000).
-    Default: 10000
+    Deposit amount to test. Default: 10000
+    Variable (int): BT_DEPOSIT
   -D (digits)
     Specify market digits (e.g. 5 or 4).
+    Variable (uint): BT_DIGITS
   -e (filename/url/pattern)
     EA name to test (TestExpert).
+    Variable (string): EXPERT
   -E (filename/url/pattern)
     EA name to run (Expert).
+    Variable (string): EXPERT
   -f (filename)
     The .set file to run the test.
+    Variable (string): SETORG
   -F
     Convert test report file to full detailed text format.
+    Variable (bool): OPT_FORMAT_FULL
   -g
-    Post results to Gist.
-    Activates: -j and -t
+    Post results to Gist. It enables: -j and -t
+    Variables (bool): OPT_GIST
   -G
     Enhance gif report files.
+    Variable (bool): OPT_GIF_ENHANCE
   -i (file)
     Invoke file with custom rules.
+    Variable (string): INCLUDE
   -I (options)
     Change tester INI file with custom settings (e.g. Server=MetaQuotes-Demo,Login=123).
+    Variable (string): TEST_OPTS
   -j
     Convert test report file into JSON format.
+    Variable (bool): OPT_FORMAT_JSON
   -l (double)
     Specify a lot step (e.g. 0.01).
+    Variable (float): BT_LOTSTEP
   -L (limit)
     EA common/limit test parameters separated by comma (e.g. genetic=0,maxdrawdown=20.00).
+    Variable (string): EA_OPTS
   -m (month)
-    Month to test (e.g. 1).
-    Default: 1-12
+    Month to test. Default: 1-12.
+    Variable (uint/string): BT_MONTHS
   -M (version)
-    Specify version of MetaTrader (e.g. 4, 4x, 5, 4.0.0.1010).
-    Default: 4.0.0.1010
+    Specify version of MetaTrader (e.g. 4, 4x, 5, 4.0.0.1010). Default: 4.0.0.1010
+  -o
+    Run test in optimization mode.
+    Variable (bool): OPT_OPTIMIZATION
+  -O (dir)
+    Output directory to save the test results. Default: /opt/results for Docker container.
+    Variable (string): BT_DEST
   -p (pair)
     Symbol pair to test (e.g. EURUSD).
   -P (param)
     Set EA param in SET file (e.g. VerboseInfo=1,TakeProfit=0).
+    Variable (string): SET_OPTS
   -r (string)
-    The name of the test report file (TestReport).
-    Default: tester/Report
+    The name of the test report file. Default: tester/Report
+    Variable (string): TEST_REPORT
   -R
     Set files to read-only.
   -s (file/url/pattern)
     Script to run (Script).
+    Variable (string): SCRIPT
   -S (spread)
     Spread to test in points.
-  -o
-    Run test in optimization mode.
-  -O (dir)
-    Output directory to save the test results.
-    Default: /opt/results for Docker container.
+    Variable (uint): BT_SPREAD
   -t
     Convert test report file into brief text format.
+    Variable (bool): OPT_FORMAT_BRIEF
   -T (timeframe)
-    Timeframe (TestPeriod) to test (e.g. M15, M30).
-    Default: M30
+    Timeframe (TestPeriod) to test. Default: M30
+    Variable (string): BT_PERIOD
   -v
     Verbose mode.
+    Variable (bool): OPT_VERBOSE
   -V
     Run test in visual mode (TestVisualEnable).
+    Variable (bool): VISUAL_MODE
   -x
     Run the script in trace/debug mode.
   -X (file)
     Invoke file on exit after the successful test.
   -y (year)
-    Year to test (e.g. 2017, 2011-2015).
-    Default: 2017
+    Year to test. Default: 2017
+    Variable (uint/string): BT_YEARS
   -?
     Display help.
+
+Other variables (without arguments assigned):
+- BT_TESTMODE (uint)
+  This controls type of backtest data being downloaded.
+  Values: 0 (default) - Every tick, 1 - Control points, 2 - Open prices only
 
 Example: $0 -v -t -e MACD -p EURUSD -c USD -d 2000 -y 2017 -m 1-2 -S 20 -b DS -T M30
 _EOF
@@ -771,7 +795,7 @@ if [ "$TEST_EXPERT" ]; then
   # Generate backtest files if not present.
   if [ -z "$(find "$TERMINAL_DIR" -name "${BT_SYMBOL}*_0.fxt" -print -quit)" ] || [ "${bt_data%.*}" != "$bt_key" ]; then
     env SERVER=$SERVER OPT_VERBOSE=$OPT_VERBOSE TRACE=$TRACE \
-      $SHELL $SCR/get_bt_data.sh $BT_SYMBOL "$(join_by - ${BT_YEARS[@]:-2017})" ${BT_SRC:-DS} ${BT_PERIOD}
+      $SHELL $SCR/get_bt_data.sh $BT_SYMBOL "$(join_by - ${BT_YEARS[@]:-2017})" ${BT_SRC:-DS} ${BT_PERIOD} ${BT_TESTMODE:-0}
     if [ "$OPT_VERBOSE" ]; then
       cd "$TERMINAL_DIR"
       find . '(' -name "*.hst" -o -name "*.fxt" ')' -ls
