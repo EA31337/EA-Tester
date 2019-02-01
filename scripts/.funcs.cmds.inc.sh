6b7f8c051b76d938a807cac3d87a78a38306eb47
kenorb <kenorb@users.noreply.github.com>
2019-02-01 23:38:23 +0000

    Executes fluxbox when installed

--- scripts/.funcs.cmds.inc.sh
+++ scripts/.funcs.cmds.inc.sh
@@ -198,6 +198,9 @@ set_display() {
   fi
   ! pgrep -a Xvfb && Xvfb $DISPLAY -screen 0 1024x768x16 &
   sleep 1
+  if which fluxbox &>/dev/null; then
+    ! pgrep -a fluxbox && fluxbox 2>/dev/null &
+  fi
 }
 
 # Detect and configure proxy.
