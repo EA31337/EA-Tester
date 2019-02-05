7617c4fa7f9bf17ba14b67da3418a69c32572f59
kenorb <kenorb@users.noreply.github.com>
2019-02-05 01:25:30 +0000

    Corrects MT4 installation process

--- scripts/install_mt4x.sh
+++ scripts/install_mt4x.sh
@@ -26,61 +26,33 @@ echo "Downloading MT4 installer..." >&2
 set_display
 live_stats &
 
-INSTALL_DONE=0
-while test "$INSTALL_DONE" -eq 0; do
-  echo "Starting MT4 Setup in Wine..." >&2
-  wine "$HOME/$EXEFILE" &
+echo "Starting MT4 Setup in Wine..." >&2
+wine "$HOME/$EXEFILE" &
 
-  echo "Waiting for Wine to initialize..." >&2
-  while ! WID=$(xdotool search --name "4 Setup"); do
-    sleep 5
-    winedbg --command "info wnd" | grep Meta || true
-  done
+echo "Waiting for installer to initialize..." >&2
+while ! WID=$(xdotool getactivewindow 2>/dev/null); do sleep 2; done
 
-  echo "Title: $(xdotool getwindowname $WID)..." >&2
-
-  echo "Sending installer keystrokes..." >&2
-  xdotool key --window $WID --delay 500 space
-
-  echo "Giving installer time to work..." >&2
+echo "Installing $(xdotool getwindowname $WID)..." >&2
+while WID=$(xdotool getactivewindow); do
+  xdotool key --window $WID --delay 500 Alt+N
   sleep 20
-
-  echo "Seeing if installer is finished..." >&2
-  xdotool key --window $WID --delay 500 space
-  sleep 10
-
-  if CWID=$(xdotool search --name '^XM UK MT4$'); then
-    echo "Installer has stalled, restarting it to try again." >&2
-    xdotool key --window $CWID --delay 500 space
-  else
-    echo "Installer has finished." >&2
-    xdotool key --window $WID --delay 500 Tab space
-    # Workaround for Chrome launching when installer finishes.
-    pkill -KILL chrome || true
-    INSTALL_DONE=1
-  fi
-
-  while winedbg --command "info wnd" | grep "MT4 Setu"; do
-    echo "Waiting for installer to exit..." >&2
-    sleep 5
-  done
+  xdotool key --window $WID --delay 500 Tab space
 done
 
-echo "Waiting for MT4 platform to start..." >&2
-while ! WID=$(xdotool search --name "MT4"); do
-  winedbg --command "info wnd $WID" | grep -vw Empty | cut -c67- | xargs
-  sleep 5
-done
+# Workaround for Chrome launching when installer finishes.
+pkill -KILL chrome || true
+#(sleep 10 && wineserver -k) &
+
+echo "Waiting for platform to start..." >&2
+while ! WID=$(xdotool getactivewindow 2>/dev/null); do sleep 5; done
 xwininfo -id $WID -tree
 
 # Close running MT4 instance, first the two login popup window, secondly application itself.
-echo "Closing application..." >&2
-xdotool key --window $WID --delay 500 Escape Escape Alt+f x
-while winedbg --command "info wnd" | grep "MetaQuotes"; do
-  echo "Waiting for application to exit..." >&2
-  sleep 5
-done
-wineserver -k
+echo "Closing platform..." >&2
+xdotool search -classname terminal.exe key --delay 200 Escape Escape Alt+f x
+
+echo "Waiting for application to exit..." >&2
+while WID=$(xdotool getactivewindow 2>/dev/null); do sleep 2; done
 
 find "$HOME" /opt -name terminal.exe -print -quit
 echo "Installation successful." >&2
