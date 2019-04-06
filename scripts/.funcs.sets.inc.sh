5220d3274e9b3c4bac109eb524adf23de9a196db
kenorb <kenorb@users.noreply.github.com>
2019-04-06 16:20:22 +0100

    Shell syntax improvements

--- scripts/.funcs.sets.inc.sh
+++ scripts/.funcs.sets.inc.sh
@@ -33,11 +33,11 @@ input_set() {
   local value="$2"
   local file="${3:-$TESTER_DIR/$EA_SETFILE}"
   [ -s "$file" ]
-  read -ra vargs <<<$EX_ARGS
+  read -ra vargs <<<"$EX_ARGS"
   vargs+=("-u NONE")
   if [ -n "$value" ]; then
     echo "Setting '$key' to '$value' in $(basename "$file")" >&2
-    ex +"%s/$key=\zs.*$/$value/" -scwq! ${vargs[@]} "$file" >&2 || exit 1
+    ex +"%s/$key=\\zs.*$/$value/" -scwq! "${vargs[@]}" "$file" >&2 || exit 1
   else
     echo "Value for '$key' is empty, ignoring." >&2
   fi
@@ -49,8 +49,8 @@ input_get() {
   local key="$1"
   local file="${2:-$TESTER_DIR/$EA_SETFILE}"
   [ -s "$file" ]
-  value="$(grep -om1 "$key=[.0-9a-zA-Z-]\+" "$file" | cut -d= -f2-)"
-  echo $value
+  value="$(grep -om1 "$key=[.0-9a-zA-Z-]\\+" "$file" | cut -d= -f2-)"
+  echo "$value"
 }
 
 # Copy input value from the SET file to MQL file.
@@ -59,8 +59,8 @@ input_copy() {
   local key=$1
   local file_src=$2
   local retries=5
-  read -ra files <<<${@:3}
-  read -ra vargs <<<$EX_ARGS
+  read -ra files <<<"${@:3}"
+  read -ra vargs <<<"$EX_ARGS"
   vargs+=("-u NONE")
   [ -s "$file_src" ]
   for file_dst in "${files[@]}"; do
@@ -68,7 +68,7 @@ input_copy() {
     value=$(input_get "^$key" "$file_src")
     echo "Setting '$key' to '$value' in $(basename "$file_dst")" >&2
     retries=5
-    while ! ex +"%s/\s${key}[^=]=[^0-9]\zs[^;]\+/${value}/" -scwq! ${vargs[@]} "$file_dst" >&2; do
+    while ! ex +"%s/\\s${key}[^=]=[^0-9]\\zs[^;]\\+/${value}/" -scwq! "${vargs[@]}" "$file_dst" >&2; do
       sleep 1
       ((retries-=1))
       echo "Retrying ($retries left)..." >&2
@@ -83,15 +83,15 @@ export_set() {
   local name=${1:-$TEST_EXPERT}
   local dstfile=${2:-${name}.set}
   local ea_path=$name
-  local ahk_path="$(winepath -w "$SCR"/ahk/export_set.ahk)"
+  ahk_path="$(winepath -w "$SCR"/ahk/export_set.ahk)"
   [ ! -s "$name" ] && ea_path=$(ea_find "${name##/}")
   [ ! -f "$EXPERTS_DIR/$ea_path" ] && { echo "Error: Cannot find EA: ${name}!" >&2; return; }
   [[ "$ea_path" =~ 'mq' ]] && compile_ea "$name" >&2
   set_display >&2
-  ini_set "^Expert" "$(basename ${ea_path/\//\\\\} .${ea_path##*.})" "$TERMINAL_INI"
+  ini_set "^Expert" "$(basename "${ea_path/\//\\\\}" ."${ea_path##*.}")" "$TERMINAL_INI"
   WINEPATH="$(winepath -w "$TERMINAL_DIR");C:\\Apps\\AHK" \
   timeout 60 \
-  wine AutoHotkey /ErrorStdOut "$ahk_path" "${dstfile}" ${@:3}
+  wine AutoHotkey /ErrorStdOut "$ahk_path" "${dstfile}" "${@:3}"
   [ -n "$OPT_VERBOSE" ] && times >&2
   echo "${dstfile}"
 }
