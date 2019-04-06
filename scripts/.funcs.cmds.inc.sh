931d40401d32e157f05619f8a324969220961782
kenorb <kenorb@users.noreply.github.com>
2019-04-06 16:15:20 +0100

    Moves SET-related functions to .funcs.sets.inc.sh

--- scripts/.funcs.cmds.inc.sh
+++ scripts/.funcs.cmds.inc.sh
@@ -384,25 +384,6 @@ compile_and_test() {
   $CWD/run_backtest.sh -e "$@"
 }
 
-# Experts SET file. Returns exported filename.
-# Usage: export_set [EA/pattern] (dst/file) (...args)
-export_set() {
-  local name=${1:-$TEST_EXPERT}
-  local dstfile=${2:-${name}.set}
-  local ea_path=$name
-  local ahk_path="$(winepath -w "$SCR"/ahk/export_set.ahk)"
-  [ ! -s "$name" ] && ea_path=$(ea_find "${name##/}")
-  [ ! -f "$EXPERTS_DIR/$ea_path" ] && { echo "Error: Cannot find EA: ${name}!" >&2; return; }
-  [[ "$ea_path" =~ 'mq' ]] && compile_ea "$name" >&2
-  set_display >&2
-  ini_set "^Expert" "$(basename ${ea_path/\//\\\\} .${ea_path##*.})" "$TERMINAL_INI"
-  WINEPATH="$(winepath -w "$TERMINAL_DIR");C:\\Apps\\AHK" \
-  timeout 60 \
-  wine AutoHotkey /ErrorStdOut "$ahk_path" "${dstfile}" ${@:3}
-  [ -n "$OPT_VERBOSE" ] && times >&2
-  echo "${dstfile}"
-}
-
 # Copy ini settings from templates.
 # Usage: ini_copy
 ini_copy() {
@@ -824,57 +805,6 @@ quick_run() {
   script_copy $scrname
   (time wine "$TERMINAL_EXE" "config/$CONF_TEST" $TERMINAL_ARG) 2>> "$TERMINAL_LOG"
   show_logs 40
-}
-
-# Set input value in the SET file.
-# Usage: input_set [key] [value] [file]
-input_set() {
-  local key="$1"
-  local value="$2"
-  local file="${3:-$TESTER_DIR/$EA_SETFILE}"
-  [ -s "$file" ]
-  read -ra vargs <<<$EX_ARGS
-  vargs+=("-u NONE")
-  if [ -n "$value" ]; then
-    echo "Setting '$key' to '$value' in $(basename "$file")" >&2
-    ex +"%s/$key=\zs.*$/$value/" -scwq! ${vargs[@]} "$file" >&2 || exit 1
-  else
-    echo "Value for '$key' is empty, ignoring." >&2
-  fi
-}
-
-# Get input value from the SET file.
-# Usage: input_get [key] [file]
-input_get() {
-  local key="$1"
-  local file="${2:-$TESTER_DIR/$EA_SETFILE}"
-  [ -s "$file" ]
-  value="$(grep -om1 "$key=[.0-9a-zA-Z-]\+" "$file" | cut -d= -f2-)"
-  echo $value
-}
-
-# Copy input value from the SET file to MQL file.
-# Usage: input_copy [key] [src_file] [dst_file] (dst_file2...)
-input_copy() {
-  local key=$1
-  local file_src=$2
-  local retries=5
-  read -ra files <<<${@:3}
-  read -ra vargs <<<$EX_ARGS
-  vargs+=("-u NONE")
-  [ -s "$file_src" ]
-  for file_dst in "${files[@]}"; do
-    [ -s "$file_dst" ]
-    value=$(input_get "^$key" "$file_src")
-    echo "Setting '$key' to '$value' in $(basename "$file_dst")" >&2
-    retries=5
-    while ! ex +"%s/\s${key}[^=]=[^0-9]\zs[^;]\+/${value}/" -scwq! ${vargs[@]} "$file_dst" >&2; do
-      sleep 1
-      ((retries-=1))
-      echo "Retrying ($retries left)..." >&2
-      [ $retries -le 0 ] && break
-    done
-  done
 }
 
 # Set value in the INI file.
