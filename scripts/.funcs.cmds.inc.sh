e24c3b946ed22601417f31f28ad6f1117b14a71e
kenorb <kenorb@users.noreply.github.com>
2019-10-02 21:33:45 +0100

    Ensures to get Server not DataServer

--- scripts/.funcs.cmds.inc.sh
+++ scripts/.funcs.cmds.inc.sh
@@ -303,7 +303,7 @@ file_copy() {
 # Copy srv files into terminal dir.
 # Usage: srv_copy [file]
 srv_copy() {
-  local server="$(ini_get Server)"
+  local server="$(ini_get ^Server)"
   srv_file=$(find "$ROOT" -name "$server.srv" -type f -print -quit)
   if [ -n "$srv_file" ]; then
     cp $VFLAG "$srv_file" "$TERMINAL_CNF"/
@@ -555,7 +555,7 @@ file_copy() {
 # Copy server files into terminal dir.
 # srv_copy
 srv_copy() {
-  local server="$(ini_get Server)"
+  local server="$(ini_get ^Server)"
   srv_file=$(find "$ROOT" -name "$server.srv" -type f -print -quit)
   if [ -n "$srv_file" ]; then
     cp $VFLAG "$srv_file" "$TERMINAL_CNF"/
