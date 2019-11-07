993b46aac06e784815a55bdac86056659fc271b4
kenorb <kenorb@users.noreply.github.com>
2019-11-07 23:35:29 +0000

    Error on undeleted objects left

--- scripts/.funcs.inc.sh
+++ scripts/.funcs.inc.sh
@@ -126,6 +126,7 @@ check_log_errors() {
   errors+=("Assert fail on .\+")
   errors+=("Testing pass stopped .\+")
   errors+=("leaked memory")
+  errors+=("undeleted objects left")
   cd "$TERMINAL_DIR"
   ! check_logs ".\+ no history data" || { ini_del "bt_data" "$CUSTOM_INI"; }
   ! eval grep --color -iw -C2 "$(printf -- '-e "%s" ' "${errors[@]}")" */*.log */*/*.log
