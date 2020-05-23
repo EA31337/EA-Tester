840b53d8697499e2b6290bb5c947d9be61769437
kenorb <kenorb@users.noreply.github.com>
2020-05-23 23:13:18 +0100

    Adds test for conv_csv_to_mt

--- scripts/tests/test_bt_data_gen.sh
+++ scripts/tests/test_bt_data_gen.sh
@@ -18,8 +18,15 @@ dst="/tmp"
 for type in none wave curve zigzag random; do
   for spread in 10 20; do
     for digits in 4 5; do
-      filename="${type}-s${spread}-d${digits}.csv"
-      bt_data_gen -p $type -o "$dst/$filename" -s $spread -D $digits 2020.01.01 2020.01.02 1.0 2.0
+      file="$dst/${type}-s${spread}-d${digits}.csv"
+      # Convert to CSV.
+      bt_data_gen -p $type -o "$file" -s $spread -D $digits 2020.01.01 2020.01.02 1.0 2.0
+      # Convert to binary format.
+      for format in fxt4 hst4 hcc; do
+        for tf in M1 M5 M15 M30 H1 H4 D1 W1 MN1; do
+          conv_csv_to_mt -i "$file" -d "$dst" -f $format -t $tf -s ${type^^} -v
+        done
+      done
     done
   done
 done
