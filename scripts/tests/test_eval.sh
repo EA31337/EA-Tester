aee314ebf999d099d2ea0e105530f4459e7f5a1e
kenorb <kenorb@users.noreply.github.com>
2020-05-23 17:57:32 +0100

    Adds test_eval.sh

---
+++ scripts/tests/test_eval.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Test eval.sh file.
+set -e
+
+# Initialize.
+. ../.funcs.inc.sh
+initialize
+
+# Includes required code.
+. ../.aliases.inc.sh
+
+# Asserts.
+bc_4=$(run bc <<<"2+2")
+[ "$bc_4" -eq 4 ]
+
+echo "${BASH_SOURCE[0]} done."
