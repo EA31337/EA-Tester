edf9346ed2b737b844b44709f838763042056b1a
kenorb <kenorb@users.noreply.github.com>
2020-06-09 23:19:03 +0100

    Adds test for finding EA/script files

--- scripts/tests/test_cmds.sh
+++ scripts/tests/test_cmds.sh
@@ -10,6 +10,19 @@ initialize
 . ../.funcs.cmds.inc.sh
 
 # Asserts.
-help
+help | grep "Usage" >/dev/null
+
+# Load variables.
+. ../.vars.inc.sh
+
+# Test finding EA files.
+[ -f "$(ea_find Dummy)" ]
+[ -f "$(MT_VER=4 ea_find Dummy.mq4)" ]
+[ -f "$(MT_VER=5 ea_find Dummy.mq5)" ]
+
+# Test finding script files.
+[ -f "$(script_find Dummy)" ]
+[ -f "$(MT_VER=4 script_find Dummy.mq4)" ]
+[ -f "$(MT_VER=5 script_find Dummy.mq5)" ]
 
 echo "${BASH_SOURCE[0]} done."
